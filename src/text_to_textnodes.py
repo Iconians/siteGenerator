445b8a853a3c4bc1a884d236325b71e887413f86
Iconians <claytoncripe@gmail.com>
2025-01-15 17:36:32 -0800

    deleted some commented code

--- src/text_to_textnodes.py
+++ src/text_to_textnodes.py
@@ -3,34 +3,6 @@ from split_nodes_delimiter import split_nodes_delimiter
 from split_nodes_image import split_nodes_image
 from split_nodes_links import split_nodes_link
 
-# def text_to_textnodes(text):
-#     nodes = [TextNode(text, TextType.NORMAL)]
-#     print("Initial nodes:", nodes)
-    
-#     nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
-#     print("After splitting bold:", nodes)
-    
-#     nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
-#     print("After splitting italic:", nodes)
-    
-#     nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
-#     print("After splitting code:", nodes)
-    
-#     nodes = split_nodes_image(nodes)
-#     print("After splitting images:", nodes)
-    
-#     nodes = split_nodes_link(nodes)
-#     print("After splitting links:", nodes)
-    
-#     return nodes
-
-# # Example usage
-# if __name__ == "__main__":
-#     text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-#     nodes = text_to_textnodes(text)
-#     for node in nodes:
-#         print(node)
-
 def text_to_textnodes(text):
     nodes = [TextNode(text, TextType.NORMAL)]
     nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
@@ -39,10 +11,3 @@ def text_to_textnodes(text):
     nodes = split_nodes_image(nodes)
     nodes = split_nodes_link(nodes)
     return nodes
-
-# # Example usage
-# if __name__ == "__main__":
-#     text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-#     nodes = text_to_textnodes(text)
-#     for node in nodes:
-#         print(node)
